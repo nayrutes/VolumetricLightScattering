fde372665b4956db4ccc0022bfddde2a21f5ee7f
Main-PC <fwavek@gmail.com>
2020-09-04 01:10:25 +0200

    Windmill Demo

---
+++ VolumetricLightScatteringHDRP/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] private GameObject go;
+
+    [SerializeField] private float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        go.transform.Rotate(Vector3.forward,speed*Time.deltaTime);
+    }
+}
